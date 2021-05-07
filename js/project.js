ea894b4fd85b29f958bf2188196275284627b267
mfahmialkautsar <mfahmiak@gmail.com>
2021-05-07 17:38:52 +0700

    Restructure files & indentation

---
+++ js/project.js
@@ -0,0 +1,72 @@
+import {default as projects} from '../data/projects.js';
+import {default as designs} from '../data/designs.js';
+
+function project() {
+  function createProjectContainer({title, img, category, desc, link}) {
+    const projects = document.createElement('div');
+    projects.classList.add('work');
+
+    const p = `
+			<a class="text-no-decor text--main card" href="${link}" target="_blank">
+					<div class="image-container app-image-container">
+							<div style="background-image: url('${img}')" class="img"></div>
+							<div class="kind text--white">${category}</div>
+					</div>
+					<div class="work-text">
+							<h3 class="title">${title}</h3>
+							<p class="desc">${desc}</p>
+					</div>
+			</a>
+			`;
+    projects.innerHTML = p;
+    return projects;
+  }
+
+  function createDesignContainer({title, img, link}) {
+    const design = document.createElement('div');
+    design.classList.add('work');
+
+    const p = `
+			<a class="text-no-decor text--main card" href="${link}" target="_blank">
+					<div class="image-container design-image-container">
+					<div style="background-image: url('${img}')" class="img"></div>
+					</div>
+					<div class="work-text">
+							<h3 class="title">${title}</h3>
+					</div>
+			</a>
+			`;
+    design.innerHTML = p;
+    return design;
+  }
+
+  let projectsSection = document.querySelector('#projects');
+  function generateProjectsBlock() {
+    const projs = projects;
+
+    projs
+      .slice()
+      .reverse()
+      .forEach((proj) => {
+        const projectContainter = createProjectContainer(proj);
+        projectsSection.appendChild(projectContainter);
+      });
+  }
+
+  let desginsSection = document.querySelector('#designs');
+  function generateDesignsBlock() {
+    const ds = designs;
+
+    ds.slice()
+      .reverse()
+      .forEach((design) => {
+        const designContainter = createDesignContainer(design);
+        desginsSection.appendChild(designContainter);
+      });
+  }
+
+  generateProjectsBlock();
+  generateDesignsBlock();
+}
+
+export default project;
---
+++ js/theme.js
@@ -0,0 +1,52 @@
+function theme() {
+  let theme = localStorage.getItem('theme');
+
+  let themeIcon = document.querySelector('.theme-switcher i');
+
+  if (theme != 'night') {
+    theme = 'day';
+    themeIcon.classList.add('fa-sun-o');
+  } else {
+    theme = 'night';
+    themeIcon.classList.add('fa-moon-o');
+  }
+  setTheme(theme);
+
+  let themeButton = document.querySelector('.theme-switcher');
+  themeButton.setAttribute('data-mode', theme);
+
+  let rotate = true;
+  themeButton.addEventListener('click', function () {
+    var mode = themeButton.getAttribute('data-mode');
+    if (mode == 'day') {
+      themeButton.setAttribute('data-mode', 'night');
+      themeIcon.classList.remove('fa-sun-o');
+      themeIcon.classList.add('fa-moon-o');
+    } else if (mode == 'night') {
+      themeButton.setAttribute('data-mode', 'day');
+      themeIcon.classList.remove('fa-moon-o');
+      themeIcon.classList.add('fa-sun-o');
+    }
+
+    if (rotate) {
+      themeIcon.classList.add('rotate');
+    } else {
+      themeIcon.classList.remove('rotate');
+    }
+
+    rotate = !rotate;
+    setTheme(this.dataset.mode);
+  });
+
+  function setTheme(mode) {
+    if (mode == 'day') {
+      document.getElementById('theme-style').href = 'css/style.css';
+    } else if (mode == 'night') {
+      document.getElementById('theme-style').href = 'css/night.css';
+    }
+
+    localStorage.setItem('theme', mode);
+  }
+}
+
+export default theme;
