2e29ea2bd3aa27b5a9df685f161a97fd7f6622af
Fahmi Al <mfahmiak@gmail.com>
2023-01-06 16:13:56 +0700

    Add SureBack to project

--- src/lib/repositories/data/projects.ts
+++ src/lib/repositories/data/projects.ts
@@ -13,9 +13,17 @@ const projects: Project[] = [
     'id': 2,
     'img': 'lexplay.jpg',
     'title': 'LexPlay',
-    'category': 'iOS',
+    'category': 'iOS, WatchOS',
     'desc': 'Pre-school dyslexic kids app to learn the alphabet by using the multi-sensory approaches',
-    'link': 'https://testflight.apple.com/join/g8eqfcQk',
+    'link': 'https://lexplay.vercel.app',
+  },
+  {
+    'id': 3,
+    'img': 'sureback.jpg',
+    'title': 'SureBack',
+    'category': 'iOS',
+    'desc': 'iOS application to track and record culinary businesses customers\' Instagram story promotion that has fair and automatic reward distribution after 24 hours',
+    'link': 'https://testflight.apple.com/join/o4INPHzX',
   },
 ];
 
